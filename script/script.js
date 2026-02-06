109da9b18962824a276d5dab226346f96e3af416
markgee624 <markgitonga647@gmail.com>
2026-02-06 18:25:43 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    alert("Welcome ,lets begin with my childhood");
+}
+
+function validateForm() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+
+    if (name === "" || email === "") {
+        alert("Please fill all fields");
+        return false;
+    }
+    alert("Message sent successfully!");
+    return true;
+}
